7b612f7680a1f98159ee6a174316cebc2468062c
hershdhillon <mail.hdhillon@gmail.com>
2024-09-15 21:25:53 -0400

    updates

--- src/Scene.js
+++ src/Scene.js
@@ -14,7 +14,7 @@ const Scene = () => {
                 {
                     id: now.getTime(),
                     time: formattedTime,
-                    position: [Math.random() * 8 - 4, 10, Math.random() * 2 - 1],
+                    position: [Math.random() * 8 - 4, 15, Math.random() * 2 - 1], // Changed Y position from 10 to 15
                 },
                 ...prevStrips.slice(0, 49),
             ])
@@ -36,8 +36,8 @@ const Scene = () => {
     }
 
     return (
-        <Physics>
-            <PerspectiveCamera makeDefault position={[0, 5, 30]} fov={30} />
+        <Physics iterations={10} tolerance={0.0001} defaultContactMaterial={{ restitution: 0.5 }}>
+            <PerspectiveCamera makeDefault position={[0, -30, 30]} fov={30} />
             <OrbitControls />
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} castShadow />
--- src/TimeStrip.js
+++ src/TimeStrip.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { Text } from '@react-three/drei'
 import { useBox } from '@react-three/cannon'
@@ -8,35 +8,42 @@ const TimeStrip = ({ id, time, initialPosition, onRemove }) => {
     const [ref, api] = useBox(() => ({
         mass: 0.1,
         position: initialPosition,
-        args: [8, 1, 0.1], // Increased size of the box
+        args: [8, 1, 0.1],
         linearDamping: 0.95,
     }))
 
+    const lifetimeRef = useRef(0)
+
+    useEffect(() => {
+        api.applyImpulse([0, -5, 0], [0, 0, 0])
+    }, [api])
+
     useFrame((state, delta) => {
-        setOpacity((prevOpacity) => {
-            const newOpacity = prevOpacity - delta * 0.05
-            if (newOpacity <= 0) {
-                onRemove(id)
-            }
-            return Math.max(newOpacity, 0)
-        })
+        lifetimeRef.current += delta
+        const newOpacity = 1 - lifetimeRef.current / 20
+        if (newOpacity <= 0) {
+            onRemove(id)
+        }
+        setOpacity(Math.max(newOpacity, 0))
     })
 
     return (
         <group ref={ref}>
             <mesh>
-                <boxGeometry args={[8, 1, 0.2]} /> {/* Increased size of the box */}
+                <boxGeometry args={[8, 1, 0.2]} />
                 <meshStandardMaterial
-                    color="black" // Changed color to black
-                    transparent={false} // Made the box opaque
+                    color="black"
+                    transparent
+                    opacity={opacity}
                 />
             </mesh>
             <Text
-                fontSize={0.4} // Increased font size
-                color="white" // Changed text color to white for contrast
+                fontSize={0.4}
+                color="white"
                 anchorX="center"
                 anchorY="middle"
                 position={[0, 0, 0.11]}
+                opacity={opacity}
             >
                 {time}
             </Text>
